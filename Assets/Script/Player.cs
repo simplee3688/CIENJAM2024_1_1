2ffec1863e4516ff5ffaca389ff2fa68b2ac8d1b
CloudNru <grutugy1025@gmail.com>
2024-06-24 20:18:12 +0900

    Fix Bug

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -16,12 +16,14 @@ public class Player : MonoBehaviour
     [SerializeField] float dashForce;
 
     [SerializeField] bool isFloor;
-    [SerializeField] int dir;
+    [SerializeField] int dir = 1;
     [SerializeField] int maxJumpCount = 1;
     [SerializeField] int maxDashCount = 1;
 
     int jumpCount = 0;
     int dashCount = 0;
+
+    Vector3 moveVec;
 
     Rigidbody2D rigid;
 
@@ -56,16 +58,14 @@ public class Player : MonoBehaviour
 
         if (Input.GetAxisRaw("Horizontal") != 0)
         {
-            int dir = Input.GetAxisRaw("Horizontal") > 0 ? 1 : -1;
-            hit = Physics2D.BoxCast(this.transform.position + dir * Vector3.right * 0.45f, new Vector2(0.12f, 0.8f), 0, Vector2.zero, 0, 1 << 3);
-            if (hit.collider != null)
-            {
-                p_x = 0;
-            }
-            else
-            {
-                p_x = Input.GetAxisRaw("Horizontal") * speed;
-            }
+            dir = Input.GetAxisRaw("Horizontal") > 0 ? 1 : -1; 
+            p_x = Input.GetAxisRaw("Horizontal") * speed;
+        }
+
+        hit = Physics2D.BoxCast(this.transform.position + dir * Vector3.right * 0.45f, new Vector2(0.15f, 0.8f), 0, Vector2.zero, 0, 1 << 3);
+        if (hit.collider != null)
+        {
+            p_x = 0;
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && isFloor && jumpCount > 0)
@@ -89,16 +89,26 @@ public class Player : MonoBehaviour
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireCube(this.transform.position - Vector3.up * 0.45f, new Vector2(0.8f, 0.12f));
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(this.transform.position + dir * Vector3.right * 0.45f, new Vector2(0.15f, 0.8f));
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(this.transform.position + Vector3.right * dir * 0.5f, this.transform.position + Vector3.right * dir * 0.5f + moveVec * Time.deltaTime);
     }
 
     private void FixedUpdate()
     {
-        if(p_x != 0)
+        moveVec = new Vector3(p_x + dashForce * dir, p_y, 0);
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position + Vector3.right * dir * 0.5f, moveVec.normalized, Vector2.Distance(Vector2.zero, moveVec * Time.deltaTime), 1 << 3);
+        if(hit)
         {
-            dir = p_x > 0 ? 1 : -1;
+            rigid.MovePosition(hit.point - dir * Vector2.right * 0.5f);
         }
-        Vector3 moveVec = new Vector3(p_x + dashForce * dir, p_y, 0);
-        rigid.MovePosition(this.transform.position + moveVec * Time.deltaTime);
+        else
+        {
+            rigid.MovePosition(this.transform.position + moveVec * Time.deltaTime);
+        }
 
         p_x *= 0.85f;
         dashForce *= 0.85f;

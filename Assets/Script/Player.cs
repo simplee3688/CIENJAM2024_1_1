eab25f60a50bfd66c49916292151156af936e7ad
CloudNru <grutugy1025@gmail.com>
2024-06-25 13:57:16 +0900

    Edit use Rigidbody2D.getContacts

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
@@ -16,16 +17,21 @@ public class Player : MonoBehaviour
     [SerializeField] int maxJumpCount = 1;
     [SerializeField] int maxDashCount = 1;
 
-    float p_x;
-    float p_y;
-    float dashForce;
+    [SerializeField] float input_x;
+    [SerializeField] float p_x;
+    [SerializeField] float p_y;
+    [SerializeField] float dashForce;
 
-    int jumpCount = 0;
-    int dashCount = 0;
+    [SerializeField] int jumpCount = 0;
+    [SerializeField] int dashCount = 0;
 
-    Vector3 moveVec;
+    [SerializeField] float groundAngle;
+    [SerializeField] float wallAngle;
 
-    Rigidbody2D rigid;
+    [SerializeField] Vector3 moveVec;
+    List<ContactPoint2D> contactList;
+
+    [SerializeField] Rigidbody2D rigid;
 
     // Start is called before the first frame update
     void Start()
@@ -35,37 +41,25 @@ public class Player : MonoBehaviour
         {
             this.rigid = this.AddComponent<Rigidbody2D>();
         }
+        contactList = new List<ContactPoint2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
-        RaycastHit2D hit = Physics2D.BoxCast(this.transform.position - Vector3.up * 0.45f, new Vector2(0.8f, 0.15f), 0, Vector2.zero, 0, 1 << 3);
-        if(hit.collider != null)
-        {
-            if (!isFloor)
-            {
-                jumpCount = maxJumpCount;
-                p_y = 0;
-                isFloor = true;
-            }
-        }
-        else
-        {
-            isFloor = false;
-        }
-
         if (Input.GetAxisRaw("Horizontal") != 0)
         {
-            dir = Input.GetAxisRaw("Horizontal") > 0 ? 1 : -1; 
-            p_x = Input.GetAxisRaw("Horizontal") * speed;
+            dir = Input.GetAxisRaw("Horizontal") > 0 ? 1 : -1;
+            input_x = Input.GetAxisRaw("Horizontal") * speed;
         }
 
-        hit = Physics2D.BoxCast(this.transform.position + dir * Vector3.right * 0.45f, new Vector2(0.15f, 0.8f), 0, Vector2.zero, 0, 1 << 3);
-        if (hit.collider != null)
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashCount > 0)
         {
-            p_x = 0;
+            dashCount--;
+            dashForce = dashPower;
+        }
+        else if (isFloor)
+        {
+            dashCount = maxDashCount;
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && jumpCount > 0)
@@ -73,49 +67,92 @@ public class Player : MonoBehaviour
             jumpCount--;
             p_y = jumpPower;
         }
+    }
 
-        if(Input.GetKeyDown(KeyCode.LeftShift) && dashCount > 0)
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float minSlide = 1;
+        rigid.GetContacts(contactList);
+        //Debug.Log("Count : " + contactList.Count);
+        if (contactList.Count > 0)
         {
-            dashCount--;
-            dashForce = dashPower;
+            float groundCos = Mathf.Cos(groundAngle / 180 * Mathf.PI);
+            float wallCos = Mathf.Cos(wallAngle / 180 * Mathf.PI);
+            bool tmpIsFloor = false;
+
+            foreach (ContactPoint2D contactPoint in contactList)
+            {
+                float groundCheck = Vector2.Dot(Vector2.up, contactPoint.normal.normalized);
+                //Debug.Log("groundChek : " + groundCheck + ", groundCos : " + groundCos);
+                float wallCheck = Vector2.Dot(Vector2.right, contactPoint.normal.normalized);
+                //Debug.Log("wallChek : " + wallCheck + ", wallCos : " + wallCos);
+                
+                if (groundCheck > groundCos)
+                {
+                    tmpIsFloor = true;
+
+                    if (!isFloor)
+                    {
+                        jumpCount = maxJumpCount;
+                        p_y = 0;
+                    }
+                }
+                else if (Mathf.Abs(wallCheck) > wallCos)
+                {
+                    if (wallCheck < 0)
+                    {
+                        input_x = Mathf.Min(0, input_x);
+                        p_x = Mathf.Min(0, p_x);
+                        dashForce = Mathf.Min(0, dashForce);
+                    }
+                    else
+                    {
+                        input_x = Mathf.Max(0, input_x);
+                        p_x = Mathf.Max(0, p_x);
+                        dashForce = Mathf.Max(0, dashForce);
+                    }
+                }
+                else if(contactPoint.normal.normalized.y > 0)
+                {
+                    minSlide = Mathf.Min(contactPoint.normal.normalized.y, minSlide);
+                    Debug.Log("Vector : " + contactPoint.normal +  ", normal Vector : " + contactPoint.normal.normalized + ", minSlide Change : " + minSlide);
+                }
+            }
+            isFloor = tmpIsFloor;
         }
-        else if(isFloor)
+        else
         {
-            dashCount = maxDashCount;
+            isFloor = false;
+            p_x *= 0.85f;
+        }
+
+       Debug.Log("p_y : " + p_y + ", isFloor : " + isFloor + ", minSlide : " + minSlide);
+        moveVec = new Vector3(input_x + p_x + dashForce * dir, p_y, 0);
+        rigid.MovePosition(this.transform.position + moveVec * Time.deltaTime);
+
+        input_x *= 0.85f;
+        dashForce *= 0.85f;
+        if (!isFloor)
+        {
+            //Debug.Log(minSlide);
+            p_y -= gravityScale * minSlide;
         }
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireCube(this.transform.position - Vector3.up * 0.45f, new Vector2(0.8f, 0.12f));
-
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireCube(this.transform.position + dir * Vector3.right * 0.45f, new Vector2(0.15f, 0.8f));
-
         Gizmos.color = Color.green;
-        Gizmos.DrawLine(this.transform.position + Vector3.right * dir * 0.5f, this.transform.position + Vector3.right * dir * 0.5f + moveVec * Time.deltaTime);
+        Gizmos.DrawLine(this.transform.position, this.transform.position + moveVec.normalized * 2);
+
+        if (rigid != null && contactList != null)
+        {
+            Gizmos.color = Color.black;
+            foreach (ContactPoint2D contactPoint in contactList)
+            {
+                Gizmos.DrawLine(contactPoint.point, contactPoint.point + contactPoint.normal * 2);
+            }
+        }
     }
 
-    private void FixedUpdate()
-    {
-        moveVec = new Vector3(p_x + dashForce * dir, p_y, 0);
-        RaycastHit2D hit = Physics2D.Raycast(this.transform.position + Vector3.right * dir * 0.5f, moveVec.normalized, Vector2.Distance(Vector2.zero, moveVec * Time.deltaTime), 1 << 3);
-        if(hit)
-        {
-            dashForce = 0;
-            rigid.MovePosition(hit.point - dir * Vector2.right * 0.5f);
-        }
-        else
-        {
-            rigid.MovePosition(this.transform.position + moveVec * Time.deltaTime);
-        }
-
-        p_x *= 0.85f;
-        dashForce *= 0.85f;
-        if (!isFloor)
-        {
-            p_y -= gravityScale;
-        }
-    }
 }

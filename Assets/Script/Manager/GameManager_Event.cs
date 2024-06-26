5e412aaed83c2116b07098700d2e3d94e82f7cfb
simplee <simplee@cau.ac.kr>
2024-06-26 15:20:47 +0900

    StartScene/GameManager_DontDontDestory

--- Assets/Script/Manager/GameManager_Event.cs
+++ Assets/Script/Manager/GameManager_Event.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public partial class GameManager : MonoBehaviour
 {
@@ -67,5 +68,8 @@ public partial class GameManager : MonoBehaviour
         else Time.timeScale = 0.0f;
     }
 
-    
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(gameObject.scene.ToString());
+    }
 }
